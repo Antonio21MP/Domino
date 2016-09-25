63a4126ada98417439b340bce8d2f845b4c6afb6
Jose34mp <jose_mejiap34@unitec.edu>
2016-09-24 23:25:26 -0600

    Board new implements

--- src/com/dominoe/board/BoardLogic.java
+++ src/com/dominoe/board/BoardLogic.java
@@ -1,12 +1,33 @@
 package com.dominoe.board;
 
 import com.dominoe.Board;
+import com.dominoe.pieces.Piece;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class BoardLogic implements Board {
     List board;
     @Override
     public void init() {
-        board = new List();
-        
+        board = new List();   
     }
+
+    @Override
+    public void pushLeft(Piece selectedPiece) {
+        try {
+            board.pushLeft(selectedPiece);
+        } catch (NotFoundPieceException | rootIsNullException ex) {
+            Logger.getLogger(BoardLogic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void pushRight(Piece selectedPiece) {
+        try {
+            board.pushRight(selectedPiece);
+        } catch (NotFoundPieceException | rootIsNullException ex) {
+            Logger.getLogger(BoardLogic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     
 }
